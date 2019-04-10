078e0cc00ff161f1868d35f088a1cf836ec18d1b
André Bocato <andrebocato@gmail.com>
2019-04-10 10:27:44 -0300

    Update RMCharacter.swift
    Creating initialiser to use mocks.

--- RickAndMortyCatalog/Model/Character/RMCharacter.swift
+++ RickAndMortyCatalog/Model/Character/RMCharacter.swift
@@ -9,16 +9,25 @@
 import Foundation
 
 struct RMCharacter: Codable {
-    var id: Int?                        /// The id of the character.
-    var name: String?                   /// The name of the character.
-    var status: String?                 /// The status of the character ('Alive', 'Dead' or 'unknown').
-    var species: String?                /// The species of the character.
-    var type: String?                   /// The type or subspecies of the character.
-    var gender: String?                 /// The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
-    var origin: RMCharacterOrigin?      /// Name and link to the character's origin location.
-    var location: RMCharacterLocation?  /// Name and link to the character's last known location endpoint.
-    var image: String?                  /// Link to the character's image. Images are 300x300px and most are medium shots or portraits. They are intended to be used as avatars.
-    var episode: [String]?              /// List of episodes in which this character appeared.
-    var url: String?                    /// Link to the character's own URL endpoint.
-    var created: String?                /// Time at which the character was created in the database.
+    let id: Int?                        /// The id of the character.
+    let name: String?                   /// The name of the character.
+    let status: String?                 /// The status of the character ('Alive', 'Dead' or 'unknown').
+    let species: String?                /// The species of the character.
+    let type: String?                   /// The type or subspecies of the character.
+    let gender: String?                 /// The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
+    let origin: RMCharacterOrigin?      /// Name and link to the character's origin location.
+    let location: RMCharacterLocation?  /// Name and link to the character's last known location endpoint.
+    let image: String?                  /// Link to the character's image. Images are 300x300px and most are medium shots or portraits. They are intended to be used as avatars.
+    let episode: [String]?              /// List of episodes in which this character appeared.
+    let url: String?                    /// Link to the character's own URL endpoint.
+    let created: String?                /// Time at which the character was created in the database.
+}
+
+extension RMCharacter {
+
+    init(id: Int, name: String){
+        self.id = id
+        self.name = name
+    }
+    
 }
